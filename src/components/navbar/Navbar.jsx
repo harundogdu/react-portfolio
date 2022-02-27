56e010ff29f7ca2d0051909e70f8567826c4ceab
Harun Doğdu <harundogdu06@gmail.com>
2022-02-28 00:37:32 +0300

    scroll method added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,6 +6,37 @@ import { RiServiceLine } from 'react-icons/ri'
 
 const Navbar = () => {
     const [isActive, setIsActive] = React.useState("#home")
+
+    React.useEffect(() => {
+        document.addEventListener("scroll", () => {
+            if (window.scrollY < window.innerHeight) {
+                if (isActive !== "#home") {
+                    setIsActive("#home")
+                }
+            }
+            if (window.scrollY > window.innerHeight && window.scrollY < window.innerHeight * 2) {
+                if (isActive !== "#about") {
+                    setIsActive('#about')
+                }
+            }
+            if (window.scrollY > window.innerHeight * 2 && window.scrollY < window.innerHeight * 3) {
+                if (isActive !== "#experience") {
+                    setIsActive("#experience")
+                }
+            }
+            if (window.scrollY > window.innerHeight * 3 && window.scrollY < window.innerHeight * 4) {
+                if (isActive !== "#services") {
+                    setIsActive("#services")
+                }
+            }
+            if (window.scrollY > window.innerHeight * 4 && window.scrollY < window.innerHeight * 5) {
+                if (isActive !== "#contact") {
+                    setIsActive("#contact")
+                }
+            }
+        })
+    }, [isActive])
+
     return (
         <nav className='navbar'>
             <ul>
